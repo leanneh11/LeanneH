6ce77bbe3090045e95310fbd4513a6a6b0723c08
Leanne Hardesty <lhardesty@razavi.com>
2021-09-18 21:31:39 -0700

    First release of Portfolio Website 9/18/21

---
+++ js/work.js
@@ -0,0 +1,83 @@
+
+// array for work examples
+
+/*
+var works = [
+"img/PortfolioExample1.jpg",
+"img/PortfolioExample2.jpg",
+"img/PortfolioExample3.jpg",
+"img/PortfolioExample4.jpg",
+"img/PortfolioExample5.jpg",
+"img/PortfolioExample7.jpg",
+"img/PortfolioExample10.jpg",
+"img/PortfolioExample1.jpg",
+"img/PortfolioExample2.jpg",
+"img/PortfolioExample3.jpg"
+]
+*/
+
+
+var works = [
+{
+	title: 	"Bus Tracker",
+	pic: 	"img/BusTracker.png",
+	prog:	"bustracker/BusTracker.html"
+	
+},
+{
+	title: 	"Eyes Exercise",
+	pic: 	"img/eyes.png",
+	prog:	"eyes/eyes.html"
+	
+},
+{
+	title: 	"PacMan",
+	pic: 	"img/pacmen.png",
+	prog:	"pacman/pacmen.html"
+	
+	
+},
+{
+	title: 	"Documents",
+	pic: 	"img/githubsetup.png",
+	prog:	"documents/documents.html"
+
+}
+
+/*
+{
+	title: 	"Ball Animation Explained",
+	pic: 	"img/ComputerImage3.jpg",
+	prog:	"ballAnimation/BallAnimationExplained.html"
+	
+
+},
+{
+	title: 	"PacMan",
+	pic: 	"img/ComputerImage4.jpg",
+	prog:	"pacman/oldindex.html"
+
+	
+
+},
+{
+	title: 	"Documents",
+	pic: 	"img/ComputerImage4.jpg",
+	prog:	"documentation/SetupInGitHub.html"
+
+},
+
+{
+	title: 	"Example1",
+	pic: 	"img/ComputerImage5.jpg"
+
+} 
+*/
+
+
+];
+
+
+
+
+
